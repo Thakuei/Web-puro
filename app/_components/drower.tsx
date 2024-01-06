397d53451b9e7dabd06e2941a68be6b383729cd7
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2024-01-07 01:02:03 +0900

    現在ログインしているUserを表示させる

--- app/_components/drower.tsx
+++ app/_components/drower.tsx
@@ -11,13 +11,34 @@ import {
   Input,
   useDisclosure,
   Center,
-  Link,
+  Box,
+  Text,
 } from '@chakra-ui/react'
 import { FiAlignJustify } from 'react-icons/fi'
+import { supabase } from '@/utils/supabaseClient';
+import { useState, useEffect } from 'react';
+
+
+interface User {
+  email?: string;
+}
 
 export default function DrawerExample() {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = React.useRef()
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        setUser(user);
+      }
+    }
+
+    fetchUser();
+  }, []);
 
   return (
     <>
@@ -28,20 +49,22 @@ export default function DrawerExample() {
       <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-        <DrawerHeader>
-
-        </DrawerHeader>
-
-        <DrawerBody>
-          <Center>
-            <Button as={"a"} href='/' mt={10} bgColor={"blue.100"} _hover={{bgColor: "gray.400"}}>ログアウト</Button>
-          </Center>
-        </DrawerBody>
-          <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose} bgColor={"blue.100"} _hover={{bgColor: "gray.400"}}>
-                閉じる
-              </Button>
-          </DrawerFooter>
+          <DrawerHeader></DrawerHeader>
+          <DrawerBody>
+            {user && (
+                <Box>
+                  <Text fontSize={10}>ログイン中のユーザー：{user.email}</Text>
+                </Box>
+              )}
+            <Center>
+              <Button as={"a"} href='/' mt={10} bgColor={"blue.100"} _hover={{bgColor: "gray.400"}}>ログアウト</Button>
+            </Center>
+          </DrawerBody>
+            <DrawerFooter>
+                <Button variant='outline' mr={3} onClick={onClose} bgColor={"blue.100"} _hover={{bgColor: "gray.400"}}>
+                  閉じる
+                </Button>
+            </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,5 @@
-'use client'
-
 import { Text, Box, Flex, Center, Button } from "@chakra-ui/react"
-import { FiAlignJustify } from "react-icons/fi";
-import Link from "next/link";
-import { FaGithub } from "react-icons/fa";
 import Drawer from '../_components/drower';
-
-
 
 export default function Header (){
 
@@ -14,11 +7,7 @@ export default function Header (){
     <Box bgColor={"white"} top={0} position={"sticky"} left={0} right={0} zIndex={"docked"}>
       <Box>
           <Drawer /> 
-        <Center>
-        </Center>
       </Box>
-      <Center>
-      </Center>
     </Box>
   )
 }
